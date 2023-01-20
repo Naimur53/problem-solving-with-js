96ca651379bd79ef38e135fedd74655c1e08cf97
naimur53 <naimurrhman53@gmail.com>
2023-01-20 19:00:37 +0600

    oe

--- zig-zack.js
+++ zig-zack.js
@@ -1,21 +1,54 @@
-const input = [2, 3, 5, 1, 4]
+const input = [1, 5, 3, 4, 6, 2, 7];
+input.sort((a, b) => a - b)
+
 let output = [];
 
 const len = input.length;
 const min = Math.min(...input)
 const max = Math.max(...input)
 
-const mid = Math.round(len / 2)
-const midNumber = input[mid];
+const mid = Math.round(max / 2)
 
 let first = []
 let last = []
 
-output[mid - 1] = max;
-input.forEach(ele => {
-    if (ele !== max) {
+output[mid] = max;
 
-    }
-})
-console.log(output)
 
+console.log(output);
+checker(input)
+
+
+function checker(arr) {
+    const len = arr.length;
+    const min = Math.min(...arr)
+    const max = Math.max(...arr)
+
+    const midPoint = Math.round(len / 2)
+
+    arr.forEach((single, i) => {
+        if (i) {
+            // increasing
+            if (i < midPoint) {
+                if (single > arr[i - 1]) {
+                    console.log('ok increasing')
+                }
+                else {
+                    return false;
+                }
+            }
+            // decreasing
+            else {
+                if (single < arr[i - 1]) {
+                    console.log('ok decreasing')
+                }
+                else {
+                    return false;
+                }
+            }
+
+        }
+
+    });
+
+}
