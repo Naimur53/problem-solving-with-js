d0b65e6e93e683777d1c00e2c0349644475416ea
naimur53 <naimurrhman53@gmail.com>
2023-01-25 15:36:16 +0600

    eo3

--- zig-zack.js
+++ zig-zack.js
@@ -1,22 +1,26 @@
-const input = [1, 5, 3, 4, 6, 2, 7];
-input.sort((a, b) => a - b)
+const input = [1, 2, 3, 4, 5, 6, 7];
+// input.sort((a, b) => a - b)
 
-let output = [];
+// let output = [];
 
-const len = input.length;
-const min = Math.min(...input)
-const max = Math.max(...input)
+// const len = input.length;
+// const min = Math.min(...input)
+// const max = Math.max(...input)
 
-const mid = Math.round(max / 2)
+// const mid = Math.round(max / 2)
 
-let first = []
-let last = []
+// let first = []
+// let last = []
 
-output[mid] = max;
+// output[mid] = max;
 
 
-console.log(output);
-checker(input)
+// console.log(output);
+// checker(input)
+// input.splice(5, 5)
+console.log(convertToZigZack(input))
+
+
 
 
 function checker(arr) {
@@ -34,6 +38,8 @@ function checker(arr) {
                     console.log('ok increasing')
                 }
                 else {
+                    console.log('wrong increasing')
+
                     return false;
                 }
             }
@@ -43,6 +49,8 @@ function checker(arr) {
                     console.log('ok decreasing')
                 }
                 else {
+                    console.log('wrong decreasing')
+
                     return false;
                 }
             }
@@ -51,4 +59,50 @@ function checker(arr) {
 
     });
 
+}
+
+
+function convertToZigZack(arr) {
+
+    short(arr)
+
+    const len = arr.length;
+    const mid = Math.round(len / 2)
+
+    console.log(mid)
+    const first = arr.slice(0, mid);
+    const firstLastNum = first[first.length - 1]
+
+    const firstEle = first.slice(0, first.length - 1)
+    console.log(firstEle)
+
+    const last = arr.slice(mid)
+
+    let lastRev = [];
+    const lastEle = [firstLastNum, ...last]
+
+    lastEle.forEach(ele => {
+        lastRev = [ele, ...lastRev]
+    })
+
+    const result = firstEle.concat(lastRev)
+    console.log(...result)
+
+}
+
+
+
+function short(arr) {
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i + 1]) {
+            if (arr[i] > arr[i + 1]) {
+
+                const tem = arr[i]
+                arr[i] = arr[i + 1];
+                arr[i + 1] = tem;
+                return short(arr);
+            }
+        }
+    }
 }
